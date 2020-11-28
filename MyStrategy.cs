13e654f6379f0115ddb6b501874befca35b6aa39
YellowFive <yellowfive555@yahoo.com>
2020-11-28 15:46:07 +0300

    Builders build

--- MyStrategy.cs
+++ MyStrategy.cs
@@ -12,6 +12,7 @@ namespace Aicup2020
         private PlayerView View { get; set; }
         private DebugInterface Debug { get; set; }
         private World Around { get; }
+        private Dictionary<int, EntityAction> Actions;
 
         public MyStrategy()
         {
@@ -20,27 +21,50 @@ namespace Aicup2020
 
         public Action GetAction(PlayerView playerView, DebugInterface debugInterface)
         {
-            var entitiesActions = new Dictionary<int, EntityAction>();
-
+            Actions = new Dictionary<int, EntityAction>();
             View = playerView;
             Debug = debugInterface;
 
             Around.Scan(View);
 
-            foreach (var builder in Around.MyUnitsBuilders)
+            CommandBuildingsBuilders();
+            CommandUnitsBuilders();
+
+            return new Action(Actions);
+        }
+
+        private void CommandBuildingsBuilders()
+        {
+            foreach (var builderBuilding in Around.MyBuildingsBuilders)
             {
-                var nearestSpice = Around.GetNearestTo(builder, PlayerType.My, EntityType.Resource);
+                var buildAction = new BuildAction();
+
+                var canBuildBuilders = Around.Me.Resource >= Around.BuilderUnitCost;
+                if (canBuildBuilders)
+                {
+                    var position = new Vec2Int(builderBuilding.Position.X + 5, builderBuilding.Position.Y);
+                    buildAction = new BuildAction(EntityType.BuilderUnit, position);
+                }
+
+                var action = new EntityAction(null, buildAction, null, null);
+
+                Actions.Add(builderBuilding.Id, action);
+            }
+        }
+
+        private void CommandUnitsBuilders()
+        {
+            foreach (var builderUnit in Around.MyUnitsBuilders)
+            {
+                var nearestSpice = Around.GetNearestTo(builderUnit, PlayerType.My, EntityType.Resource);
 
                 var moveAction = new MoveAction(nearestSpice.Position, true, false);
                 var attackAction = new AttackAction(nearestSpice.Id, null);
 
                 var action = new EntityAction(moveAction, null, attackAction, null);
 
-                entitiesActions.Add(builder.Id, action);
+                Actions.Add(builderUnit.Id, action);
             }
-
-
-            return new Action(entitiesActions);
         }
 
         public void DebugUpdate(PlayerView playerView, DebugInterface debugInterface)
--- World.cs
+++ World.cs
@@ -11,17 +11,19 @@ namespace Aicup2020
 {
     public class World
     {
-        public IEnumerable<Entity> SpiceMilange { get; private set; }
 
+        public int BuilderUnitCost { get; private set; }
+        public IEnumerable<Entity> SpiceMilange { get; private set; }
+        
         #region Enemy
 
         public IEnumerable<Player> EnemyPlayers { get; private set; }
         public IEnumerable<Entity> EnemyEntities { get; private set; }
-        public IEnumerable<Entity> EnemyBasesWalls => EnemyEntities.Where(e => e.EntityType == EntityType.Wall).ToArray();
-        public IEnumerable<Entity> EnemyBasesHouses => EnemyEntities.Where(e => e.EntityType == EntityType.House).ToArray();
-        public IEnumerable<Entity> EnemyBasesBuilders => EnemyEntities.Where(e => e.EntityType == EntityType.BuilderBase).ToArray();
-        public IEnumerable<Entity> EnemyBasesMelees => EnemyEntities.Where(e => e.EntityType == EntityType.MeleeBase).ToArray();
-        public IEnumerable<Entity> EnemyBasesRanged => EnemyEntities.Where(e => e.EntityType == EntityType.RangedBase).ToArray();
+        public IEnumerable<Entity> EnemyBuildingsWalls => EnemyEntities.Where(e => e.EntityType == EntityType.Wall).ToArray();
+        public IEnumerable<Entity> EnemyBuildingsHouses => EnemyEntities.Where(e => e.EntityType == EntityType.House).ToArray();
+        public IEnumerable<Entity> EnemyBuildingsBuilders => EnemyEntities.Where(e => e.EntityType == EntityType.BuilderBase).ToArray();
+        public IEnumerable<Entity> EnemyBuildingsMelees => EnemyEntities.Where(e => e.EntityType == EntityType.MeleeBase).ToArray();
+        public IEnumerable<Entity> EnemyBuildingsRanged => EnemyEntities.Where(e => e.EntityType == EntityType.RangedBase).ToArray();
         public IEnumerable<Entity> EnemyUnitsTurrets => EnemyEntities.Where(e => e.EntityType == EntityType.Turret).ToArray();
         public IEnumerable<Entity> EnemyUnitsBuilders => EnemyEntities.Where(e => e.EntityType == EntityType.BuilderUnit).ToArray();
         public IEnumerable<Entity> EnemyUnitsMelees => EnemyEntities.Where(e => e.EntityType == EntityType.MeleeUnit).ToArray();
@@ -31,12 +33,13 @@ namespace Aicup2020
 
         #region Me
 
+        public Player Me { get; private set; }
         public IEnumerable<Entity> MyEntities { get; private set; }
-        public IEnumerable<Entity> MyBasesWalls => MyEntities.Where(e => e.EntityType == EntityType.Wall).ToArray();
-        public IEnumerable<Entity> MyBasesHouses => MyEntities.Where(e => e.EntityType == EntityType.House).ToArray();
-        public IEnumerable<Entity> MyBasesBuilders => MyEntities.Where(e => e.EntityType == EntityType.BuilderBase).ToArray();
-        public IEnumerable<Entity> MyBasesMelees => MyEntities.Where(e => e.EntityType == EntityType.MeleeBase).ToArray();
-        public IEnumerable<Entity> MyBasesRanged => MyEntities.Where(e => e.EntityType == EntityType.RangedBase).ToArray();
+        public IEnumerable<Entity> MyBuildingsWalls => MyEntities.Where(e => e.EntityType == EntityType.Wall).ToArray();
+        public IEnumerable<Entity> MyBuildingsHouses => MyEntities.Where(e => e.EntityType == EntityType.House).ToArray();
+        public IEnumerable<Entity> MyBuildingsBuilders => MyEntities.Where(e => e.EntityType == EntityType.BuilderBase).ToArray();
+        public IEnumerable<Entity> MyBuildingsMelees => MyEntities.Where(e => e.EntityType == EntityType.MeleeBase).ToArray();
+        public IEnumerable<Entity> MyBuildingsRanged => MyEntities.Where(e => e.EntityType == EntityType.RangedBase).ToArray();
         public IEnumerable<Entity> MyUnitsTurrets => MyEntities.Where(e => e.EntityType == EntityType.Turret).ToArray();
         public IEnumerable<Entity> MyUnitsBuilders => MyEntities.Where(e => e.EntityType == EntityType.BuilderUnit).ToArray();
         public IEnumerable<Entity> MyUnitsMelees => MyEntities.Where(e => e.EntityType == EntityType.MeleeUnit).ToArray();
@@ -46,10 +49,13 @@ namespace Aicup2020
 
         public void Scan(PlayerView view)
         {
+            Me = view.Players.Single(p => p.Id == view.MyId);
             EnemyPlayers = view.Players.Where(p => p.Id != view.MyId).ToArray();
             SpiceMilange = view.Entities.Where(e => e.EntityType == EntityType.Resource).ToArray();
             EnemyEntities = view.Entities.Where(e => e.PlayerId != view.MyId && e.EntityType != EntityType.Resource).ToArray();
             MyEntities = view.Entities.Where(e => e.PlayerId == view.MyId).ToArray();
+
+            BuilderUnitCost = view.EntityProperties.Single(ep => ep.Key == EntityType.BuilderUnit).Value.Cost;
         }
 
         public Entity GetNearestTo(Entity sourceEntity, PlayerType playerType, EntityType type)
@@ -62,25 +68,25 @@ namespace Aicup2020
                     switch (type)
                     {
                         case EntityType.Wall:
-                            targetCollection = MyBasesWalls;
+                            targetCollection = MyBuildingsWalls;
                             break;
                         case EntityType.House:
-                            targetCollection = MyBasesHouses;
+                            targetCollection = MyBuildingsHouses;
                             break;
                         case EntityType.BuilderBase:
-                            targetCollection = MyBasesBuilders;
+                            targetCollection = MyBuildingsBuilders;
                             break;
                         case EntityType.BuilderUnit:
                             targetCollection = MyUnitsBuilders;
                             break;
                         case EntityType.MeleeBase:
-                            targetCollection = MyBasesMelees;
+                            targetCollection = MyBuildingsMelees;
                             break;
                         case EntityType.MeleeUnit:
                             targetCollection = MyUnitsMelees;
                             break;
                         case EntityType.RangedBase:
-                            targetCollection = MyBasesRanged;
+                            targetCollection = MyBuildingsRanged;
                             break;
                         case EntityType.RangedUnit:
                             targetCollection = MyUnitsRanged;
@@ -100,25 +106,25 @@ namespace Aicup2020
                     switch (type)
                     {
                         case EntityType.Wall:
-                            targetCollection = EnemyBasesWalls;
+                            targetCollection = EnemyBuildingsWalls;
                             break;
                         case EntityType.House:
-                            targetCollection = EnemyBasesHouses;
+                            targetCollection = EnemyBuildingsHouses;
                             break;
                         case EntityType.BuilderBase:
-                            targetCollection = EnemyBasesBuilders;
+                            targetCollection = EnemyBuildingsBuilders;
                             break;
                         case EntityType.BuilderUnit:
                             targetCollection = EnemyUnitsBuilders;
                             break;
                         case EntityType.MeleeBase:
-                            targetCollection = EnemyBasesMelees;
+                            targetCollection = EnemyBuildingsMelees;
                             break;
                         case EntityType.MeleeUnit:
                             targetCollection = EnemyUnitsMelees;
                             break;
                         case EntityType.RangedBase:
-                            targetCollection = EnemyBasesRanged;
+                            targetCollection = EnemyBuildingsRanged;
                             break;
                         case EntityType.RangedUnit:
                             targetCollection = EnemyUnitsRanged;
