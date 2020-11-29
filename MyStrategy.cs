92e8f766f5402ac01f0e6d8820d2a969de151f15
YellowFive <yellowfive555@yahoo.com>
2020-11-29 19:32:46 +0300

    1.7 house build in right places

--- MyStrategy.cs
+++ MyStrategy.cs
@@ -85,33 +85,121 @@ namespace Aicup2020
 
         private void CommandUnitsWorkers()
         {
-            var lstXY = new List<int> {-1, 1};
-
-            foreach (var builderUnit in Around.MyUnitsWorkers)
+            foreach (var builderUnit in Around.MyUnitsWorkers) // Go get spice milange
             {
-                if (Around.NeedBuildHouse && Around.CanBuildHouse)
-                {
-                    var position = new Vec2Int(builderUnit.Position.X + lstXY.ElementAt(new Random().Next(lstXY.Count)),
-                                               builderUnit.Position.Y + lstXY.ElementAt(new Random().Next(lstXY.Count)));
-                    actions.Add(builderUnit.Id, new EntityAction(null, new BuildAction(EntityType.House, position), null, null));
-                }
-                else
-                {
-                    var nearestSpice = Around.GetNearestEntityOfType(builderUnit, PlayerType.My, EntityType.Resource);
+                var nearestSpice = Around.GetNearestEntityOfType(builderUnit, PlayerType.My, EntityType.Resource);
 
-                    var moveAction = new MoveAction(nearestSpice.Position, true, false);
-                    var attackAction = new AttackAction(nearestSpice.Id, null);
-                    var action = new EntityAction(moveAction, null, attackAction, null);
+                var moveAction = new MoveAction(nearestSpice.Position, true, false);
+                var attackAction = new AttackAction(nearestSpice.Id, null);
+                var action = new EntityAction(moveAction, null, attackAction, null);
 
-                    actions.Add(builderUnit.Id, action);
-                }
+                actions.Add(builderUnit.Id, action);
             }
 
-            foreach (var brokenBuilding in Around.MyBuildingsBroken)
+            if (Around.NeedBuildHouse && Around.CanBuildHouse) // New houses needs
+            {
+                var workerToBuildHouse = Around.MyUnitsWorkers.Where(w => // Find worker who can build right now and here
+                                                                      {
+                                                                          var leftTop = new List<Vec2Int>();
+                                                                          for (var x = 0; x < 3; x++)
+                                                                          {
+                                                                              for (var y = 0; y < 3; y++)
+                                                                              {
+                                                                                  leftTop.Add(new Vec2Int(w.Position.X + x, w.Position.Y + y + 1));
+                                                                              }
+                                                                          }
+
+                                                                          var rightTop = new List<Vec2Int>();
+                                                                          for (var x = 0; x < 3; x++)
+                                                                          {
+                                                                              for (var y = 0; y < 3; y++)
+                                                                              {
+                                                                                  rightTop.Add(new Vec2Int(w.Position.X + x + 1, w.Position.Y - y + 1));
+                                                                              }
+                                                                          }
+
+                                                                          var rightBottom = new List<Vec2Int>();
+                                                                          for (var x = 0; x < 3; x++)
+                                                                          {
+                                                                              for (var y = 0; y < 3; y++)
+                                                                              {
+                                                                                  rightBottom.Add(new Vec2Int(w.Position.X - x + 1, w.Position.Y - y));
+                                                                              }
+                                                                          }
+
+                                                                          var leftBottom = new List<Vec2Int>();
+                                                                          for (var x = 0; x < 3; x++)
+                                                                          {
+                                                                              for (var y = 0; y < 3; y++)
+                                                                              {
+                                                                                  leftBottom.Add(new Vec2Int(w.Position.X - x, w.Position.Y + y));
+                                                                              }
+                                                                          }
+
+                                                                          return !leftTop.Any(s => Around.NotFreeSpace.Any(np => np.X == s.X && np.Y == s.Y)) && !rightTop.Any(s => Around.NotFreeSpace.Any(np => np.X == s.X && np.Y == s.Y)) ||
+                                                                                 !rightTop.Any(s => Around.NotFreeSpace.Any(np => np.X == s.X && np.Y == s.Y)) && !rightBottom.Any(s => Around.NotFreeSpace.Any(np => np.X == s.X && np.Y == s.Y)) ||
+                                                                                 !rightBottom.Any(s => Around.NotFreeSpace.Any(np => np.X == s.X && np.Y == s.Y)) && !leftBottom.Any(s => Around.NotFreeSpace.Any(np => np.X == s.X && np.Y == s.Y)) ||
+                                                                                 !leftBottom.Any(s => Around.NotFreeSpace.Any(np => np.X == s.X && np.Y == s.Y)) && !leftTop.Any(s => Around.NotFreeSpace.Any(np => np.X == s.X && np.Y == s.Y));
+                                                                      }).FirstOrDefault();
+                var pointToBuild = new Vec2Int();
+                var leftTop = new List<Vec2Int>();
+                for (var x = 0; x < 3; x++)
+                {
+                    for (var y = 0; y < 3; y++)
+                    {
+                        leftTop.Add(new Vec2Int(workerToBuildHouse.Position.X + x, workerToBuildHouse.Position.Y + y + 1));
+                    }
+                }
+                var rightTop = new List<Vec2Int>();
+                for (var x = 0; x < 3; x++)
+                {
+                    for (var y = 0; y < 3; y++)
+                    {
+                        rightTop.Add(new Vec2Int(workerToBuildHouse.Position.X + x + 1, workerToBuildHouse.Position.Y - y + 1));
+                    }
+                }
+                var rightBottom = new List<Vec2Int>();
+                for (var x = 0; x < 3; x++)
+                {
+                    for (var y = 0; y < 3; y++)
+                    {
+                        rightBottom.Add(new Vec2Int(workerToBuildHouse.Position.X - x + 1, workerToBuildHouse.Position.Y - y));
+                    }
+                }
+                var leftBottom = new List<Vec2Int>();
+                for (var x = 0; x < 3; x++)
+                {
+                    for (var y = 0; y < 3; y++)
+                    {
+                        leftBottom.Add(new Vec2Int(workerToBuildHouse.Position.X - x, workerToBuildHouse.Position.Y + y));
+                    }
+                }
+                if (!rightTop.Any(s => Around.NotFreeSpace.Any(np => np.X == s.X && np.Y == s.Y)))
+                {
+                    pointToBuild = new Vec2Int(workerToBuildHouse.Position.X, workerToBuildHouse.Position.Y + 1);
+                }
+                else if (!leftTop.Any(s => Around.NotFreeSpace.Any(np => np.X == s.X && np.Y == s.Y)))
+                {
+                    pointToBuild = new Vec2Int(workerToBuildHouse.Position.X - 3, workerToBuildHouse.Position.Y);
+                }
+                else if (!rightBottom.Any(s => Around.NotFreeSpace.Any(np => np.X == s.X && np.Y == s.Y)))
+                {
+                    pointToBuild = new Vec2Int(workerToBuildHouse.Position.X + 1, workerToBuildHouse.Position.Y);
+                }
+                else if (!leftBottom.Any(s => Around.NotFreeSpace.Any(np => np.X == s.X && np.Y == s.Y)))
+                {
+                    pointToBuild = new Vec2Int(workerToBuildHouse.Position.X - 3, workerToBuildHouse.Position.Y - 1);
+                }
+
+                actions.Remove(workerToBuildHouse.Id);
+                actions.Add(workerToBuildHouse.Id, new EntityAction(null, new BuildAction(EntityType.House, pointToBuild), null, null));
+            }
+
+            foreach (var brokenBuilding in Around.MyBuildingsBroken) // Repair needs
             {
                 var nearestBuilder = Around.GetNearestEntityOfType(brokenBuilding, PlayerType.My, EntityType.BuilderUnit);
                 actions.Remove(nearestBuilder.Id);
-                
+
                 var moveAction = new MoveAction(brokenBuilding.Position, true, false);
                 var repairAction = new RepairAction(brokenBuilding.Id);
                 actions.Add(nearestBuilder.Id, new EntityAction(moveAction, null, null, repairAction));
